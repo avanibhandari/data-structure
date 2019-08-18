69aef33d266f1b7b15f727fab745f3699cc7088c
avanibhandari <53971403+avanibhandari@users.noreply.github.com>
2019-08-18 12:07:29 -0700

    Create linearsearch.cpp

---
+++ linearsearch.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a[10],i,n,p,count=0;
+  cout<<"enter size of an array";
+  cin>>n;
+  cout<<"enter element in array";
+  for(i=0;i<n;i++)
+  {
+    cin>>a[i];
+      }
+   cout<<"enter element to search";
+   cin>>p; 
+   for(i=0;i<n;i++)
+   
+   {
+     if(a[i]==p)
+  
+   }
+   count++;
+
+
+   if(a[i]==0)
+   {
+     cout<<"no nt found";
+   }
+   else
+  // cout<<"no found";
+   for(i=0;i<n;i++)
+   {
+     cout<<a[i] ;
+  }
+  return 0;
+    }
+
+   
